afc027077a8ea9efc14b6d51ca333d30ec848cfe
ZhannaIvashova <zhannaklepikova90@gmail.com>
2025-01-03 20:28:39 +0300

    feat: add task getIndexOf

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -242,8 +242,16 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let index = 0;
+  while (index < str.length) {
+    const currentChar = str[index];
+    if (currentChar === letter) {
+      return index;
+    }
+    index += 1;
+  }
+  return '-1';
 }
 
 /**
